ab2e0b9f69b62637d28e753aaa5600f0b3e31335
PresentKim <qjrmckd@gmail.com>
2018-07-28 15:18:03 +0900

    Reformat code

--- src/onebone/boat/Main.php
+++ src/onebone/boat/Main.php
@@ -2,89 +2,88 @@
 
 namespace onebone\boat;
 
-use pocketmine\plugin\PluginBase;
-use pocketmine\event\Listener;
-use pocketmine\inventory\BigShapelessRecipe;
-use pocketmine\item\Item;
-use pocketmine\entity\Entity;
-use pocketmine\event\server\DataPacketReceiveEvent;
-use pocketmine\network\protocol\InteractPacket;
-use pocketmine\network\protocol\SetEntityLinkPacket;
-use pocketmine\network\protocol\MovePlayerPacket;
-use pocketmine\event\player\PlayerQuitEvent;
-
+use onebone\boat\entity\Boat;
 use onebone\boat\item\Boat as BoatItem;
 use onebone\boat\packet\PlayerInputPacket;
-use onebone\boat\entity\Boat;
+use pocketmine\entity\Entity;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\event\server\DataPacketReceiveEvent;
+use pocketmine\inventory\BigShapelessRecipe;
+use pocketmine\item\Item;
+use pocketmine\network\protocol\{
+	InteractPacket, MovePlayerPacket, SetEntityLinkPacket
+};
+use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase implements Listener{
-  private $riding = [];
+	private $riding = [];
 
-  public function onEnable(){
-    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+	public function onEnable(){
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 
-    Item::$list[333] = BoatItem::class;
-    Item::addCreativeItem(new Item(333));
-    $this->getServer()->addRecipe((new BigShapelessRecipe(Item::get(333, 0, 1)))->addIngredient(Item::get(Item::WOODEN_PLANK, null, 5))->addIngredient(Item::get(Item::WOODEN_SHOVEL, null, 1)));
+		Item::$list[333] = BoatItem::class;
+		Item::addCreativeItem(new Item(333));
+		$this->getServer()->addRecipe((new BigShapelessRecipe(Item::get(333, 0, 1)))->addIngredient(Item::get(Item::WOODEN_PLANK, null, 5))->addIngredient(Item::get(Item::WOODEN_SHOVEL, null, 1)));
 
-    Entity::registerEntity("\\onebone\\boat\\entity\\Boat", true);
+		Entity::registerEntity("\\onebone\\boat\\entity\\Boat", true);
 
-    $this->getServer()->getNetwork()->registerPacket(0xae, PlayerInputPacket::class);
-  }
+		$this->getServer()->getNetwork()->registerPacket(0xae, PlayerInputPacket::class);
+	}
 
-  public function onQuit(PlayerQuitEvent $event){
-    if(isset($this->riding[$event->getPlayer()->getName()])){
-      unset($this->riding[$event->getPlayer()->getName()]);
-    }
-  }
+	public function onQuit(PlayerQuitEvent $event){
+		if(isset($this->riding[$event->getPlayer()->getName()])){
+			unset($this->riding[$event->getPlayer()->getName()]);
+		}
+	}
 
-  public function onPacketReceived(DataPacketReceiveEvent $event){
-    $packet = $event->getPacket();
-    $player = $event->getPlayer();
-    if($packet instanceof InteractPacket){
-      $boat = $player->getLevel()->getEntity($packet->target);
-      if($boat instanceof Boat){
-        if($packet->action === 1){
-          $pk = new SetEntityLinkPacket();
-          $pk->from = $boat->getId();
-          $pk->to = $player->getId();
-          $pk->type = 2;
+	public function onPacketReceived(DataPacketReceiveEvent $event){
+		$packet = $event->getPacket();
+		$player = $event->getPlayer();
+		if($packet instanceof InteractPacket){
+			$boat = $player->getLevel()->getEntity($packet->target);
+			if($boat instanceof Boat){
+				if($packet->action === 1){
+					$pk = new SetEntityLinkPacket();
+					$pk->from = $boat->getId();
+					$pk->to = $player->getId();
+					$pk->type = 2;
 
-          $this->getServer()->broadcastPacket($player->getLevel()->getPlayers(), $pk);
-          $pk = new SetEntityLinkPacket();
-          $pk->from = $boat->getId();
-          $pk->to = 0;
-          $pk->type = 2;
-          $player->dataPacket($pk);
+					$this->getServer()->broadcastPacket($player->getLevel()->getPlayers(), $pk);
+					$pk = new SetEntityLinkPacket();
+					$pk->from = $boat->getId();
+					$pk->to = 0;
+					$pk->type = 2;
+					$player->dataPacket($pk);
 
-          $this->riding[$player->getName()] = $packet->target;
-        }elseif($packet->action === 3){
-          $pk = new SetEntityLinkPacket();
-          $pk->from = $boat->getId();
-          $pk->to = $player->getId();
-          $pk->type = 3;
+					$this->riding[$player->getName()] = $packet->target;
+				}elseif($packet->action === 3){
+					$pk = new SetEntityLinkPacket();
+					$pk->from = $boat->getId();
+					$pk->to = $player->getId();
+					$pk->type = 3;
 
-          $this->getServer()->broadcastPacket($player->getLevel()->getPlayers(), $pk);
-          $pk = new SetEntityLinkPacket();
-          $pk->from = $boat->getId();
-          $pk->to = 0;
-          $pk->type = 3;
-          $player->dataPacket($pk);
+					$this->getServer()->broadcastPacket($player->getLevel()->getPlayers(), $pk);
+					$pk = new SetEntityLinkPacket();
+					$pk->from = $boat->getId();
+					$pk->to = 0;
+					$pk->type = 3;
+					$player->dataPacket($pk);
 
-          if(isset($this->riding[$event->getPlayer()->getName()])){
-            unset($this->riding[$event->getPlayer()->getName()]);
-          }
-        }
-      }
-    }elseif($packet instanceof MovePlayerPacket){
-      if(isset($this->riding[$player->getName()])){
-        $boat = $player->getLevel()->getEntity($this->riding[$player->getName()]);
-        if($boat instanceof Boat){
-          $boat->x = $packet->x;
-          $boat->y = $packet->y;
-          $boat->z = $packet->z;
-        }
-      }
-    }
-  }
+					if(isset($this->riding[$event->getPlayer()->getName()])){
+						unset($this->riding[$event->getPlayer()->getName()]);
+					}
+				}
+			}
+		}elseif($packet instanceof MovePlayerPacket){
+			if(isset($this->riding[$player->getName()])){
+				$boat = $player->getLevel()->getEntity($this->riding[$player->getName()]);
+				if($boat instanceof Boat){
+					$boat->x = $packet->x;
+					$boat->y = $packet->y;
+					$boat->z = $packet->z;
+				}
+			}
+		}
+	}
 }
--- src/onebone/boat/entity/Boat.php
+++ src/onebone/boat/entity/Boat.php
@@ -3,63 +3,63 @@
 namespace onebone\boat\entity;
 
 use pocketmine\entity\Entity;
-use pocketmine\network\protocol\AddEntityPacket;
-use pocketmine\event\entity\EntityDeathEvent;
-use pocketmine\Player;
 use pocketmine\event\entity\EntityDamageEvent;
-use pocketmine\network\protocol\EntityEventPacket;
 use pocketmine\item\Item;
+use pocketmine\network\protocol\{
+	AddEntityPacket, EntityEventPacket
+};
+use pocketmine\Player;
 
 class Boat extends Entity{
-  const NETWORK_ID = 90;
+	const NETWORK_ID = 90;
 
-  public function spawnTo(Player $player){
-    $pk = new AddEntityPacket();
-    $pk->eid = $this->getId();
-    $pk->type = self::NETWORK_ID;
-    $pk->x = $this->x;
-    $pk->y = $this->y;
-    $pk->z = $this->z;
-    $pk->speedX = 0;
-    $pk->speedY = 0;
-    $pk->speedZ = 0;
-    $pk->yaw = 0;
-    $pk->pitch = 0;
-    $pk->metadata = $this->dataProperties;
-    $player->dataPacket($pk);
+	public function spawnTo(Player $player){
+		$pk = new AddEntityPacket();
+		$pk->eid = $this->getId();
+		$pk->type = self::NETWORK_ID;
+		$pk->x = $this->x;
+		$pk->y = $this->y;
+		$pk->z = $this->z;
+		$pk->speedX = 0;
+		$pk->speedY = 0;
+		$pk->speedZ = 0;
+		$pk->yaw = 0;
+		$pk->pitch = 0;
+		$pk->metadata = $this->dataProperties;
+		$player->dataPacket($pk);
 
-    parent::spawnTo($player);
-  }
+		parent::spawnTo($player);
+	}
 
-  public function attack($damage, EntityDamageEvent $source){
-    parent::attack($damage, $source);
+	public function attack($damage, EntityDamageEvent $source){
+		parent::attack($damage, $source);
 
-    if(!$source->isCancelled()){
-      $pk = new EntityEventPacket();
-  		$pk->eid = $this->id;
-  		$pk->event = EntityEventPacket::HURT_ANIMATION;
-      foreach($this->getLevel()->getPlayers() as $player){
-        $player->dataPacket($pk);
-      }
-    }
-  }
+		if(!$source->isCancelled()){
+			$pk = new EntityEventPacket();
+			$pk->eid = $this->id;
+			$pk->event = EntityEventPacket::HURT_ANIMATION;
+			foreach($this->getLevel()->getPlayers() as $player){
+				$player->dataPacket($pk);
+			}
+		}
+	}
 
-  public function kill(){
-    parent::kill();
+	public function kill(){
+		parent::kill();
 
 		foreach($this->getDrops() as $item){
 			$this->getLevel()->dropItem($this, $item);
 		}
-  }
+	}
 
-  public function getDrops(){
-    return [
-      Item::get(333, 0, 1)
-    ];
-  }
+	public function getDrops(){
+		return [
+			Item::get(333, 0, 1)
+		];
+	}
 
-  public function getSaveId(){
-    $class = new \ReflectionClass(static::class);
-    return $class->getShortName();
-  }
+	public function getSaveId(){
+		$class = new \ReflectionClass(static::class);
+		return $class->getShortName();
+	}
 }
