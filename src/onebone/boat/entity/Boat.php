d55a49bfac9042fb5d69e4a23da7b9a944478275
PresentKim <qjrmckd@gmail.com>
2018-07-28 16:26:02 +0900

    BoatEntity : Set max hp on initEntity()

--- src/onebone/boat/entity/Boat.php
+++ src/onebone/boat/entity/Boat.php
@@ -10,6 +10,13 @@ use pocketmine\network\mcpe\protocol\EntityEventPacket;
 class Boat extends Entity{
 	const NETWORK_ID = 90;
 
+	public function initEntity() : void{
+		$this->setMaxHealth(4);
+		//TODO: Set Entity::DATA_RIDER_SEAT_POSITION
+
+		parent::initEntity();
+	}
+
 	public function attack(EntityDamageEvent $source) : void{
 		parent::attack($source);
 
