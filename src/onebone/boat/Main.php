73b7f0b970d5965407c61645c488f42daad6ca1e
Song ki ho <44698603+GodVas@users.noreply.github.com>
2021-02-26 15:19:48 +0900

    Modified the onload and onable parts.
    - [Remove] registerRecipe
    - [Change] Entity::registerEntity moved to onLoad method.
    - [Change] ItemFactory::registerItem moved to onLaod method.
    - [Add] Add items to the creative inventory.

--- src/onebone/boat/Main.php
+++ src/onebone/boat/Main.php
@@ -12,20 +12,19 @@ use pocketmine\item\ItemFactory;
 use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase{
-
-    public function onLoad(): void{
-        Entity::registerEntity(BoatEntity::class, true);
-        ItemFactory::registerItem($item = new BoatItem(), true);
-        if (!Item::isCreativeItem($item))
-            Item::addCreativeItem($item);
-    }
-
+	
+	public function onLoad(): void{
+		Entity::registerEntity(BoatEntity::class, true);
+		ItemFactory::registerItem($item = new BoatItem(), true);
+		if (!Item::isCreativeItem($item))
+			Item::addCreativeItem($item);
+	}
+	
 	public function onEnable() : void{
 		$this->getServer()->getCraftingManager()->registerShapelessRecipe(new ShapelessRecipe([
-            Item::get(Item::WOODEN_PLANKS, 0, 5),
-            Item::get(Item::WOODEN_SHOVEL, 0, 1)
-        ], [Item::get(Item::BOAT, 0, 1)]))
-        ;
+			Item::get(Item::WOODEN_PLANKS, 0, 5),
+			Item::get(Item::WOODEN_SHOVEL, 0, 1)
+		], [Item::get(Item::BOAT, 0, 1)]));
 		$this->getServer()->getPluginManager()->registerEvents(new EventListener(), $this);
 	}
 }
