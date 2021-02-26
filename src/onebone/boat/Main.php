25cf4ff938835b33ac6977084dd8630c08cafe8b
Song ki ho <44698603+GodVas@users.noreply.github.com>
2021-02-26 15:00:07 +0900

    Source updated successfully.

--- src/onebone/boat/Main.php
+++ src/onebone/boat/Main.php
@@ -20,12 +20,11 @@ class Main extends PluginBase{
             Item::addCreativeItem($item);
     }
 
-	public function onEnable() : void{
-		$this->getServer()->getCraftingManager()->registerShapelessRecipe(new ShapelessRecipe([
+    public function onEnable() : void{
+	$this->getServer()->getCraftingManager()->registerShapelessRecipe(new ShapelessRecipe([
             Item::get(Item::WOODEN_PLANKS, 0, 5),
             Item::get(Item::WOODEN_SHOVEL, 0, 1)
-        ], [Item::get(Item::BOAT, 0, 1)]))
-        ;
-		$this->getServer()->getPluginManager()->registerEvents(new EventListener(), $this);
-	}
+        ], [Item::get(Item::BOAT, 0, 1)]));
+	$this->getServer()->getPluginManager()->registerEvents(new EventListener(), $this);
+    }
 }
